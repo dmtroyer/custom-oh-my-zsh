d890ddea26deddcf3e916104a5b236efc5039cba
David Troyer <dmtroyer@gmail.com>
2018-03-26 14:22:49 -0400

    added code, modified chrome

--- jhome.zsh
+++ jhome.zsh
@@ -1,6 +1,5 @@
 jhome() {
     export JAVA_HOME=`/usr/libexec/java_home $@`
     echo "JAVA_HOME: $JAVA_HOME"
-    echo "java -version:"
     java -version
 }
--- other.zsh
+++ other.zsh
@@ -1,3 +1,16 @@
-alias chrome="open -a 'Google Chrome'"
+alias chrome="/Applications/Google\\ \\Chrome.app/Contents/MacOS/Google\\ \\Chrome"
 alias forms="cd ~/Projects/cmu/forms"
+alias intranet="cd ~/Projects/cmu/intranet && jhome -v 1.7 && sdk use grails 2.2.5"
 alias weather="curl http://wttr.in"
+alias brewup="brew update && brew upgrade && brew cleanup"
+
+function code {
+    if [[ $# = 0 ]]
+    then
+        open -a "Visual Studio Code"
+    else
+        local argPath="$1"
+        [[ $1 = /* ]] && argPath="$1" || argPath="$PWD/${1#./}"
+        open -a "Visual Studio Code" "$argPath"
+    fi
+}
