3353d44dcc1a555a5b0688778a293a8275531528
David Troyer <DTroyer@uwhealth.org>
2019-05-10 16:33:55 -0500

    added resource

--- other.zsh
+++ other.zsh
@@ -1,7 +1,8 @@
 alias chrome="/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"
 alias chrome-proxy="chrome --proxy-server=\"socks5://127.0.0.1:8080\" --host-resolver-rules=\"MAP * 0.0.0.0 , EXCLUDE localhost\" --proxy-bypass-list=\"ai-test.uwhis.hosp.wisc.edu\" &>/dev/null &"
-alias weather="curl http://wttr.in"
 alias brewup="brew update && brew upgrade && brew cleanup"
+alias resource="source ~/.zshrc"
+alias weather="curl http://wttr.in"
 
 function code {
     if [[ $# = 0 ]]
