3832a19074a04e1aa9f92a55899c86b4f85a4356
David Troyer <dmtroyer@gmail.com>
2024-11-24 14:50:30 -0600

    remove code

--- mac.zsh
+++ mac.zsh
@@ -1,13 +1,2 @@
 alias brewup="brew update && brew upgrade && brew cleanup"
 alias myip4="dig TXT CH +short whoami.cloudflare @1.1.1.1"
-
-function code {
-    if [[ $# = 0 ]]
-    then
-        open -a "Visual Studio Code"
-    else
-        local argPath="$1"
-        [[ $1 = /* ]] && argPath="$1" || argPath="$PWD/${1#./}"
-        open -a "Visual Studio Code" "$argPath"
-    fi
-}
